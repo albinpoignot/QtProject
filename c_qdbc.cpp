52dd1e36556711a3423ada353494d542b752268f
albinpoignot <mithrop@gmail.com>
2012-03-07 12:14:00 +0100

    Ajout des premières méthodes dans c_qdbc.*

--- c_qdbc.cpp
+++ c_qdbc.cpp
@@ -1,5 +1,134 @@
 #include "c_qdbc.h"
 
-c_qdbc::c_qdbc()
+static QSqlDatabase db;
+
+//static bool isDatabaseReady();
+
+void C_qdbc::initDB(QString database)
 {
+    db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(database);
+
+    if(db.open())
+    {
+        QStringList tablesList = db.tables();
+
+        if(tablesList.length() <= 0)
+        {
+
+            QSqlQuery query = QSqlQuery();
+            bool res;
+            res = query.exec("CREATE TABLE poi(categorie VARCHAR(255), nom VARCHAR(255), long INTEGER NOT NULL, lat INTEGER NOT NULL, UNIQUE(long,lat))");
+
+            if(!res)
+            {
+                qDebug() << "*** ERREUR *** Initialisation de la BDD impossible ! ";
+            }
+        }
+        db.close();
+    }
+    else
+    {
+        qDebug() << "*** ERREUR *** impossible d'ouvrir la BDD' ! ";
+    }
 }
+
+void C_qdbc::addPoi(C_poi point)
+{
+    if(db.open())
+    {
+        QSqlQuery query;
+        QString queryText = "INSERT INTO poi(categorie, nom, long, lat) VALUES('";
+
+        queryText.append(point.getCat());
+        queryText.append("','");
+        queryText.append(point.getNom());
+        queryText.append("',");
+        queryText.append(QString().setNum(point.getPoint().rx(), 'g'));
+        queryText.append(",");
+        queryText.append(QString().setNum(point.getPoint().ry(), 'g'));
+        queryText.append(")");
+
+        bool res = query.exec(queryText);
+
+        if(!res)
+        {
+            qDebug() << "*** ERREUR *** Impossible d'ajouter le point : " << point.getNom() << " !";
+        }
+
+        db.close();
+    }
+}
+
+C_poi C_qdbc::getPoi(double lon, double lat)
+{
+    C_poi point;
+
+    if(db.open())
+    {
+        QSqlQuery query;
+        QString queryText = "SELECT * FROM poi WHERE long = " + QString::number(lon) + " AND lat = " + QString::number(lat);
+
+        bool res = query.exec(queryText);
+
+        if(!res)
+        {
+            qDebug() << "*** ERREUR *** Impossible d'obtenir le point : " << QString::number(lon) << " - " << QString::number(lat) << " !";
+        }
+        else
+        {
+            query.next();
+
+            point = C_poi();
+            point.setPoint(QPointF(lon, lat));
+            point.setCat(query.value(0).toString());
+            point.setNom(query.value(1).toString());
+        }
+
+        db.close();
+    }
+
+    return point;
+}
+
+QList<C_poi> C_qdbc::getAllPoi()
+{
+    QList<C_poi> points;
+    C_poi point;
+
+    if(db.open())
+    {
+        QSqlQuery query;
+        QString queryText = "SELECT * FROM poi";
+
+        bool res = query.exec(queryText);
+
+        if(!res)
+        {
+            qDebug() << "*** ERREUR *** Impossible d'obtenir les points !";
+        }
+        else
+        {
+            points = QList<C_poi>();
+
+            while(query.next())
+            {
+                point = C_poi();
+
+                point.setCat(query.value(0).toString());
+                point.setNom(query.value(1).toString());
+                point.setPoint(QPointF(query.value(2).toFloat(), query.value(3).toFloat()));
+
+                points.append(point);
+            }
+        }
+
+        db.close();
+    }
+
+    return points;
+}
+
+/*static void updatePoi(double lon, double lat, C_poi point);
+
+static void deletePoi(double lon, double lat);*/
--- c_qdbc.h
+++ c_qdbc.h
@@ -1,13 +1,67 @@
 #ifndef C_QDBC_H
 #define C_QDBC_H
 
-#include
+#include "c_poi.h"
 
-class c_qdbc
+#include <QString>
+#include <QStringList>
+#include <QDebug>
+
+#include <QVariant>
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+class C_qdbc
 {
-public:
-    c_qdbc();
+private:
 
+    /**
+      * \brief Retourne TRUE si la BDD est prête, FALSE sinon
+      */
+    static bool isDatabaseReady();
+
+public:
+
+    /**
+      * \brief Initialise une base de données
+      * @param database : chemin de la bdd (ex: /home/user/project/database.db)
+      */
+    static void initDB(QString database);
+
+    /**
+      * \brief Ajout d'un POI
+      * @param point : POI a ajouter
+      */
+    static void addPoi(C_poi point);
+
+    /**
+      * \brief Obtention d'un POI
+      * @param lon : longitude du point voulu
+      * @param lat : latitude du point voulu
+      */
+    static C_poi getPoi(double lon, double lat);
+
+    /**
+      * \brief Obtention de tous les POI
+      */
+    static QList<C_poi> getAllPoi();
+
+    /**
+      * \brief Mise à jour d'un POI
+      * @param lon : longitude du point à modifier
+      * @param lat : latitude du point à modifier
+      * @param point : nouveau point (lon et lat ne doivent pas changer)
+      */
+    static void updatePoi(double lon, double lat, C_poi point);
+
+    /**
+      * \brief Suppression d'un POI
+      * @param lon : longitude du point à supprimer
+      * @param lat : latitude du point à supprimer
+      */
+    static void deletePoi(double lon, double lat);
 };
 
 #endif // C_QDBC_H
